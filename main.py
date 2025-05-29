fb9f6c80ec571be088a985162d3044399152429c
shaxzo17 <shaxzodaavalboyeva14@gmail.com>
2025-05-29 10:45:36 +0300

    5_3 homework

---
+++ main.py
@@ -0,0 +1,49 @@
+# def fix_the_meerkat(arr):
+#     return arr[::-1]
+# arr = [15 , 74 , 2 ,3]
+#
+# def reverse_list(l):
+#     return l[::-1]
+# l = [2 , 5 , 2, 4]
+#
+# def invert(lst):
+#     return [-x for x in lst]
+# lst = [-4 , 4 , -6 , 7 , 0]
+#
+# def square_sum(numbers):
+#     return sum(x**2 for x in numbers)
+# numbers = [2 , 8 ,5]
+
+# def sorter(textbooks):
+#     return sorted(textbooks, key=str.lower)
+# textbooks = ["Math", "english", "Science", "history"]
+# print(sorter(textbooks))
+
+# def flick_switch(lst):
+#     result = []
+#     current = True
+#
+#     for item in lst:
+#         if item == 'flick':
+#             current = not current
+#         result.append(current)
+#     return result
+
+# def say_hello(name, city, state):
+#     full_name = ' '.join(name)
+#     return f"Hello, {full_name}! Welcome to {city}, {state}!"
+
+# def two_sort(array):
+#     sort_array = sorted(array)
+#     first = sort_array[0]
+#     result = '***'.join(first)
+#     return result
+
+# def is_vow(inp):
+#     return [chr(i) if i in [97, 101, 105, 111, 117] else i for i in inp]
+
+# def remove_exclamation_marks(s):
+#     return s.replace('!', '')
+
+
+
