2e9b3629f65a1b2541f8d2fe8ea585ec1ab16cf8
shaxzo17 <shaxzodaavalboyeva14@gmail.com>
2025-05-29 13:59:12 +0300

    Update main.py with additional functions

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
+# My head is at the wrong end!
 # def fix_the_meerkat(arr):
 #     return arr[::-1]
 # arr = [15 , 74 , 2 ,3]
 #
+
+# Reverse List Order
 # def reverse_list(l):
 #     return l[::-1]
 # l = [2 , 5 , 2, 4]
 #
+
+# Invert values
 # def invert(lst):
 #     return [-x for x in lst]
 # lst = [-4 , 4 , -6 , 7 , 0]
 #
+
+# Square(n) Sum
 # def square_sum(numbers):
 #     return sum(x**2 for x in numbers)
 # numbers = [2 , 8 ,5]
 
+# Sort My Textbooks
 # def sorter(textbooks):
 #     return sorted(textbooks, key=str.lower)
 # textbooks = ["Math", "english", "Science", "history"]
 # print(sorter(textbooks))
 
+# Flick Switch
 # def flick_switch(lst):
 #     result = []
 #     current = True
@@ -29,19 +38,25 @@
 #         result.append(current)
 #     return result
 
+
+# Welcome to the City
 # def say_hello(name, city, state):
 #     full_name = ' '.join(name)
 #     return f"Hello, {full_name}! Welcome to {city}, {state}!"
 
+# two_sort
 # def two_sort(array):
 #     sort_array = sorted(array)
 #     first = sort_array[0]
 #     result = '***'.join(first)
 #     return result
 
+
+# Is there a vowel in there?
 # def is_vow(inp):
 #     return [chr(i) if i in [97, 101, 105, 111, 117] else i for i in inp]
 
+# Remove exclamation marks
 # def remove_exclamation_marks(s):
 #     return s.replace('!', '')
 
